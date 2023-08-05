b733d16daad680c6093f06d2c46ac5cfbd4cf0db
Guerth Manzala <guerthmanzala@gmail.com>
2023-08-05 18:06:12 +0100

    feat: authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-export const revalidate = 60;
+//export const revalidate = 60;
 
 const Posts = async () => {
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      auth: {
-        persistSession: false,
-      },
-    }
-  );
+  const supabase = createServerComponentClient({
+    cookies,
+  });
 
   const { data: posts } = await supabase.from("posts").select();
   return <pre>{JSON.stringify(posts, null, 2)}</pre>;
