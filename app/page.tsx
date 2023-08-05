7ddc612d1f83b2b2f9e77597fd27d09bc5d07291
Guerth Manzala <guerthmanzala@gmail.com>
2023-08-05 17:28:57 +0100

    feat: fetch posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-const Posts = () => {
-  return <h1 className="font-bold">Posts</h1>;
+import { createClient } from "@supabase/supabase-js";
+
+export const revalidate = 60;
+
+const Posts = async () => {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: false,
+      },
+    }
+  );
+
+  const { data: posts } = await supabase.from("posts").select();
+  return <pre>{JSON.stringify(posts, null, 2)}</pre>;
 };
 
 export default Posts;
